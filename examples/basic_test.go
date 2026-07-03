18ca7be96386a7d02aeb428a385848e7531446ec
nicerobot <nicerobot@users.noreply.github.com>
2026-07-03 15:54:39 -0700

    init

---
+++ examples/basic_test.go
@@ -0,0 +1,96 @@
+package comm_test
+
+import (
+	gloo "github.com/gloo-foo/framework/patterns"
+
+	command "github.com/gloo-foo/cmd-comm"
+)
+
+// ExampleComm_basic shows the default behavior comparing two sorted files.
+// Output has 3 columns:
+//   - Column 1: lines only in file1
+//   - Column 2: lines only in file2 (indented with tab)
+//   - Column 3: lines in both files (indented with two tabs)
+func ExampleComm_basic() {
+	if err := gloo.Run(
+		command.Comm("testdata/fruits1.txt", "testdata/fruits2.txt"),
+	); err != nil {
+		panic(err)
+	}
+	// Output:
+	// apple
+	// 		banana
+	// 		cherry
+	// 	kiwi
+}
+
+// ExampleComm_suppressColumn1 hides lines unique to file1.
+// Shows only: lines unique to file2 + common lines
+func ExampleComm_suppressColumn1() {
+	if err := gloo.Run(
+		command.Comm("testdata/fruits1.txt", "testdata/fruits2.txt", command.CommSuppressColumn1),
+	); err != nil {
+		panic(err)
+	}
+	// Output:
+	// 	banana
+	// 	cherry
+	// kiwi
+}
+
+// ExampleComm_suppressColumn2 hides lines unique to file2.
+// Shows only: lines unique to file1 + common lines
+func ExampleComm_suppressColumn2() {
+	if err := gloo.Run(
+		command.Comm("testdata/fruits1.txt", "testdata/fruits2.txt", command.CommSuppressColumn2),
+	); err != nil {
+		panic(err)
+	}
+	// Output:
+	// apple
+	// 	banana
+	// 	cherry
+}
+
+// ExampleComm_suppressColumn3 hides common lines.
+// Shows only: lines unique to each file
+func ExampleComm_suppressColumn3() {
+	if err := gloo.Run(
+		command.Comm("testdata/fruits1.txt", "testdata/fruits2.txt", command.CommSuppressColumn3),
+	); err != nil {
+		panic(err)
+	}
+	// Output:
+	// apple
+	// 	kiwi
+}
+
+// ExampleComm_commonOnly shows only lines appearing in both files.
+// This is done by suppressing columns 1 and 2.
+func ExampleComm_commonOnly() {
+	if err := gloo.Run(
+		command.Comm(
+			"testdata/fruits1.txt", "testdata/fruits2.txt",
+			command.CommSuppressColumn1,
+			command.CommSuppressColumn2,
+		),
+	); err != nil {
+		panic(err)
+	}
+	// Output:
+	// banana
+	// cherry
+}
+
+// ExampleComm_uniqueOnly shows only lines unique to either file.
+// This is done by suppressing column 3 (common lines).
+func ExampleComm_uniqueOnly() {
+	if err := gloo.Run(
+		command.Comm("testdata/fruits1.txt", "testdata/fruits2.txt", command.CommSuppressColumn3),
+	); err != nil {
+		panic(err)
+	}
+	// Output:
+	// apple
+	// 	kiwi
+}
